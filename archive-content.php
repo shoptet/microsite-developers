b579f3fbe39a3cb039db633a164235981fa1a080
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2019-02-20 17:01:59 +0100

    Inital developers.shoptet template

---
+++ archive-content.php
@@ -0,0 +1,88 @@
+<?php /* Template Name: Archive Content */ ?>
+
+<?php if( !isset($singleView) || $singleView ) { get_header(); }  ?>
+
+<section class="section section-primary">
+    <div class="section-inner container">
+    <?php if( !isset($singleView) || $singleView ) { get_template_part( 'template-parts/utils/content', 'breadcrumb' ); ?>
+
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+
+    <?php } else { ?>
+
+        <h2><?php echo apply_filters( 'get_the_title', $page->post_title ); ?></h2>
+        <?php echo apply_filters( 'the_content', $page->post_content ); ?>
+
+    <?php } ?>
+
+    <?php
+
+    $query_ID = ($page->ID) ? $page->ID : $post->ID;
+
+    $args = array(
+        'post_type'      => 'page',
+        'posts_per_page' => -1,
+        'post_parent'    => $query_ID,
+        'order'          => 'ASC',
+        'orderby'        => 'menu_order'
+     );
+
+
+    $parent = new WP_Query( $args );
+
+    if ( $parent->have_posts() ) : ?>
+
+        <div>
+
+            <?php
+            $counter = 0;
+            while ( $parent->have_posts() ) : $parent->the_post();
+                if ($counter % 2 == 0):
+                echo '</div><div class="row row-indented"><!--row -->';
+                endif; ?>
+
+                <div class="col-12 col-md-6 homepage-archive-excerpt">
+
+                    <h3><?php the_title(); ?></h3>
+
+                    <?php the_excerpt(); ?>
+
+                    <?php
+
+                    $post_id = get_the_ID();
+                    $more_button_href = get_post_meta($post_id, 'more_button_href', true);
+                    $more_button_text = get_post_meta($post_id, 'more_button_text', true);
+
+                    if($more_button_href && $more_button_text) { ?>
+
+
+                    <a href="<?php echo $more_button_href; ?>" target="_blank" class="btn btn-secondary">
+                        <?php echo __($more_button_text, 'shoptet-wp-theme'); ?>
+                    </a>
+
+                    <?php
+                    } else { ?>
+
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="btn btn-secondary">
+                        <?php echo __('Přejít na článek', 'shoptet-wp-theme'); ?>
+                    </a>
+
+                    <?php } ?>
+
+                </div>
+
+            <?php $counter++; ?>
+
+            <?php endwhile; ?>
+
+        </div>
+
+    <?php endif; wp_reset_postdata(); ?>
+
+    <?php if( !isset($singleView) || $singleView ) { get_template_part( 'template-parts/page/content', 'widget' ); }  ?>
+
+    </div>
+</section>
+
+<?php if( !isset($singleView) || $singleView ) { get_footer(); }  ?>
---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Register widgets
+ */
+function shp_api_widgets_init() {
+    register_sidebar( array(
+        'name'          => 'Contact Page Half',
+        'id'            => 'contact_page_half',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+    ) );
+}
+add_action( 'widgets_init', 'shp_api_widgets_init' );
+
+add_filter('bloginfo', 'do_shortcode');
+
+add_post_type_support( 'page', 'excerpt' );
+
+if (get_option( 'close_comments_for_old_posts' ) && get_option( 'close_comments_days_old' ) == 0) {
+    add_filter('comments_open', '__return_false', 20, 2);
+    add_filter('pings_open', '__return_false', 20, 2);
+}
+
+function custom_excerpt_length($length) {
+    return 25;
+}
+add_filter('excerpt_length', 'custom_excerpt_length');
+
+function content_replace_text($text) {
+    $replace = array(
+        // 'WORD TO REPLACE' => 'REPLACE WORD WITH THIS'
+        '<table>' => '<table class="table table-bordered table-striped">',
+    );
+    $text = str_replace(array_keys($replace), $replace, $text);
+    return $text;
+}
+add_filter('the_content', 'content_replace_text');
+
+// Register and load the navigation widget
+function wpb_load_widget() {
+    register_widget( 'page_navigation_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
+
+class page_navigation_widget extends WP_Widget {
+    function __construct() {
+        parent::__construct(
+        'page_navigation_widget',
+        __('Page Navigation Widget', 'page_navigation_widget_domain'),
+        array( 'description' => __( 'Add navigation to page', 'page_navigation_widget_domain' ), )
+        );
+    }
+
+    public function widget( $args, $instance ) {
+        the_post_navigation (
+            array (
+                'prev_text' => '&laquo; <span>%title</span>',
+                'next_text' => '<span>%title</span> &raquo;',
+            )
+        );
+    }
+} // page_navigation_widget end
+?>
+
